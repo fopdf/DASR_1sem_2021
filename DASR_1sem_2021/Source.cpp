7e69a97a549fceee4c54a72c69d129d91007b64e
fopdf <58892396+fopdf@users.noreply.github.com>
2021-02-10 17:19:32 +0300

    add changes in branch

---
+++ DASR_1sem_2021/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int multi(int a, int b)
+{
+	return a * b;
+};
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	cout << a + b << ' ';
+
+	//add smth
+	int c = 3;
+	cout << a + b + c << ' ';
+	//Index editor 
+	int d = 0;
+	d = multi(a, b);
+	cout << d << ' ';
+	//add changes on github.com
+	int e[15];
+	for(int i = 0; i < 15; i++)
+	{
+		e[i] = i;
+		cout << e[i] << ' ';
+	}
+	//add changes in branch
+	for (int i = 0; i < 15; i++)
+	{
+		cout << e[i] * b << ' ';
+	}
+}
+
